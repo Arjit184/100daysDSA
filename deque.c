3ca107ccb170ba226204c5bf7a68f66819f94040
Arjit <arjitpathak805@gmail.com>
2026-03-10 23:28:26 +0530

    100

---
+++ deque.c
@@ -0,0 +1,118 @@
+/*
+5 10 20 30
+Deleted: 5
+Deleted: 30
+10 20*/
+
+#include <stdio.h>
+#define MAX 5
+
+int deque[MAX];
+int front = -1;
+int rear = -1;
+
+void push_front(int x) {
+    if ((front == 0 && rear == MAX-1) || (front == rear + 1)) {
+        printf("Deque Overflow\n");
+        return;
+    }
+
+    if (front == -1) {
+        front = rear = 0;
+    }
+    else if (front == 0) {
+        front = MAX - 1;
+    }
+    else {
+        front--;
+    }
+
+    deque[front] = x;
+}
+
+void push_back(int x) {
+    if ((front == 0 && rear == MAX-1) || (front == rear + 1)) {
+        printf("Deque Overflow\n");
+        return;
+    }
+
+    if (front == -1) {
+        front = rear = 0;
+    }
+    else if (rear == MAX-1) {
+        rear = 0;
+    }
+    else {
+        rear++;
+    }
+
+    deque[rear] = x;
+}
+
+void pop_front() {
+    if (front == -1) {
+        printf("Deque Underflow\n");
+        return;
+    }
+
+    printf("Deleted: %d\n", deque[front]);
+
+    if (front == rear) {
+        front = rear = -1;
+    }
+    else if (front == MAX-1) {
+        front = 0;
+    }
+    else {
+        front++;
+    }
+}
+
+void pop_back() {
+    if (front == -1) {
+        printf("Deque Underflow\n");
+        return;
+    }
+
+    printf("Deleted: %d\n", deque[rear]);
+
+    if (front == rear) {
+        front = rear = -1;
+    }
+    else if (rear == 0) {
+        rear = MAX - 1;
+    }
+    else {
+        rear--;
+    }
+}
+
+void display() {
+    if (front == -1) {
+        printf("Deque is empty\n");
+        return;
+    }
+
+    int i = front;
+    while (i != rear) {
+        printf("%d ", deque[i]);
+        i = (i + 1) % MAX;
+    }
+    printf("%d\n", deque[rear]);
+}
+
+int main() {
+    push_back(10);
+    push_back(20);
+    push_front(5);
+    push_back(30);
+
+    display();
+
+    pop_front();
+    pop_back();
+
+    display();
+
+    return 0;
+}
