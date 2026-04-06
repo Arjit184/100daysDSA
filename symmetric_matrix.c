808410ac96b35da836cf16bd11ff3b0d151324da
Arjit <arjitpathak805@gmail.com>
2026-04-06 17:44:58 +0530

    change

--- symmetric_matrix.c
+++ symmetric_matrix.c
@@ -12,7 +12,7 @@ int main() {
     int m,n;
 
     printf("Enter the number of rows and columns:");
-    scanf("%d %d",&m,&n)
+    scanf("%d %d",&m,&n);
 
     printf("Enter the elements of the matrix:\n");
 
@@ -20,7 +20,7 @@ int main() {
 
     for(int i=0;i<m;i++) {
         for(int j=0;j<n;j++) {
-            scanf("%d",&A[i][j]);
+            scanf("%d",&A[i][j])
         }
     }
 
