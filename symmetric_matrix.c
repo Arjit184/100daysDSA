c435e47973e947eb1f2cb53a9489bbe8da7b0c61
Arjit <arjitpathak805@gmail.com>
2026-04-12 22:02:35 +0530

    change

--- symmetric_matrix.c
+++ symmetric_matrix.c
@@ -14,15 +14,16 @@ int main() {
     printf("Enter the number of rows and columns:");
     scanf("%d %d",&m,&n)
 
-    printf("Enter the elements of the matrix:\n");
+    printf("Enter the elements of the matrix:
+        \n");
 
     int A[m][n];
 
     for(int i=0;i<m;i++) {
         for(int j=0;j<n;j++) {
-            scanf("%d",&A[i][j]);
+            scanf("%d",&A[i][j])
+
         }
-    
 
         if(m!=n) {
             printf("The matrix is not square matrix,so it cannot be symmetric amtrix as well .\n");
